cdd943759b82b20bb3c378d19e83d1f790625004
DonVeds <donveds@gmail.com>
2018-03-25 15:15:37 +0500

    configured server and mongo

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const favicon = require('serve-favicon');
 
-const { db, passport } = require('./shared/services');
+const db = require('./shared/services/db');
 const config = require('./shared/config');
 
 const main = require('./main');
 const admin = require('./admin');
 
-const flash = require('./shared/middleware/flash');
+// const flash = require('./shared/middleware/flash');
 
 const server = express();
 
@@ -26,28 +23,8 @@ server.locals = Object.assign(server.locals, config);
 server.use(express.static(config.paths.public));
 server.use('/lib', express.static(config.paths.lib));
 server.use(express.urlencoded({ extended: false }));
-server.use(favicon(config.paths.favicon));
-server.use(session({
-  name: 'sessionId',
-  secret: config.sessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    // secure: true,
-    signed: true,
-    maxAge: 1000 * 60 * 60 * 24 * 3 // 3 days
-  },
-  store: new MongoStore({
-    mongooseConnection: db.connection,
-    ttl: 60 * 60 * 24 * 3, // 3 days
-    touchAfter: 60 * 60 * 24 // 1 day
-  })
-}));
 
-server.use(passport.initialize());
-server.use(passport.session());
-server.use(flash());
+// server.use(flash());
 
 server.use((req, res, next) => {
   res.locals.user = req.user;
---
+++ shared/services/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+
+mongoose.Promise = global.Promise;
+
+mongoose.set('debug', true);
+
+mongoose.connect(config.mongodbUrl);
+
+mongoose.connection.on('error', () => console.error.bind(console, 'Mongoose error: '));
+mongoose.connection.on('open', () => console.log('Mongoose is working'));
+mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'));
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected throught app termination');
+    process.exit(0);
+  });
+});
+
+module.exports = { connection: mongoose.connection };
