b4ad238c4e46453108516b8dfde3bf99f9244671
Dmitry <donveds@gmail.com>
2019-07-15 15:30:48 +0300

    WordPress samples

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+remove_filter('the_content', 'wrautop');
+remove_filter('the_excerpt', 'wrautop');
+remove_filter('comment_text', 'wrautop');
+
+?>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+	<head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>Лидер-Сервис</title>
+        <meta name="Description" content="автосервис" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+            name="Keywords"
+            content="ремонт дизелей, ремонт форсунок, диагностика дизелей"
+        />
+        <meta name="robots" content="nofollow" />
+        <link rel="icon" href="https://leader-service.pro/wp-content/uploads/2019/06/favicon.ico" />
+        <script src="https://use.fontawesome.com/5a177d2745.js"></script>
+        <link
+            href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=cyrillic"
+            rel="stylesheet"
+        />
+	<link href="<?= bloginfo('template_directory'); ?>/main.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<!--[if lt IE 8]>
+            <p class="browserupgrade">
+                You are using an <strong>outdated</strong> browser. Please
+                <a href="http://browsehappy.com/">upgrade your browser</a> to
+                improve your experience.
+            </p>
+        <![endif]-->
+        <header class="header">
+            <a href="/" class="logo">ЛИДЕР-СЕРВИС</a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" /><label
+                class="menu-icon"
+                for="menu-btn"
+                ><span class="navicon"></span
+            ></label>
+            <ul class="menu">
+                <li><a href="#services">Чем мы занимаемся</a></li>
+                <li><a href="#about">Почему мы лучшие</a></li>
+                <li><a href="#price">Наши цены</a></li>
+                <li><a href="#contact">Контакты</a></li>
+            </ul>
+        </header>
